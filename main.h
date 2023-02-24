825ae018f611f31ad37163ad2d64d9538e1c1f05
PESAMS <120557321+PESAMS15@users.noreply.github.com>
2023-02-24 22:19:07 +0000

    Update and rename 0x04-more_functions_nested_loops/holberton.h to main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef main_h_INCLUDED
+#define main_h_INCLUDED
+
+int _isupper(int c)
+int _isdigit(int c);
+void print_alphabet_x10(void);
+int mul(int a, int b)
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+int fizz_buzz(int a, int b);
+void print_triangle(int size);
+
+#endif
